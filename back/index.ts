e15d2ced2c0ccf83424fc2973ca032a47a6971af
Lucas Frezzini <luquiitas.f@gmail.com>
2024-10-27 22:52:49 -0300

    Cleanup Index imports and older consolelogs

--- back/index.ts
+++ back/index.ts
@@ -1,31 +1,9 @@
 import express from "express";
 import cors from "cors";
-import { firestoreDB, realtimeDB } from "./src/db/database";
-import { generateRandomString } from "./src/utils/utils";
-import { v4 as uuidv4 } from "uuid";
 import { authRouter } from "./src/routes/authRoutes";
 import { userRouter } from "./src/routes/userRoutes";
 import { roomsRouter } from "./src/routes/roomsRouter";
 import { errorMiddleware } from "./src/middlewares/errorMiddleware";
-
-// Base de datos
-const usuarios = [
-  {
-    id: 1,
-    username: "Lucas",
-  },
-  {
-    id: 2,
-    username: "Santiago",
-  },
-  {
-    id: 3,
-    username: "Tano",
-  },
-];
-
-// Referencias DB
-const roomsRef = firestoreDB.collection("rooms");
 
 const app = express();
 const port = 3000;
@@ -42,10 +20,3 @@ app.use(errorMiddleware);
 app.listen(port, () => {
   console.log(`El servidor se está ejecutando desde el puerto: ${port}`);
 });
-
-// posible routes
-
-// post /rooms
-// get /rooms/:roomID
-// get /rooms/:roomID?userID=1234
-// post /rooms/choices
