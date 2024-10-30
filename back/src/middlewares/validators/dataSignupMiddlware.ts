2960ba615ad729393b5ac4f9fadd9f68b4cf71a4
Lucas Frezzini <luquiitas.f@gmail.com>
2024-10-30 16:57:04 -0300

    fix: add email validation regex

--- back/src/middlewares/validators/dataSignupMiddlware.ts
+++ back/src/middlewares/validators/dataSignupMiddlware.ts
@@ -8,13 +8,19 @@ export const dataSignupValidator = async (req: any, res: any, next: any) => {
   const { name, email } = req.body;
 
   if (!name || !email) {
-    return next(new ValidationError());
+    return next(new ValidationError("Faltan campos obligatorios"));
+  }
+
+  //Verificar que el email sea valido
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return next(new ValidationError("El email proporcionado no es valido"));
   }
 
   // Verificar si existe un usuario con ese mismo email
   let user = await usersRef.where("email", "==", email).get();
   if (user.docs.length > 0) {
-    return next(new AuthError("El usuario ya existe con esos datos"));
+    return next(new AuthError("El usuario ya existe con esos datos. Logueate"));
   }
 
   next();
