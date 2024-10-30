d20729979029c95365ed66d0fe7668c7b77f77c4
Lucas Frezzini <luquiitas.f@gmail.com>
2024-10-30 16:19:11 -0300

    refactor: Add handled localStorage to preserve state and load in the init

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,5 +1,4 @@
 import "@/style.css";
-import { handleRoute } from "./router/router";
 import "@components/button/button";
 import "@components/bottomHands/bottomHands";
 import "@components/counter/counter";
@@ -7,10 +6,16 @@ import "@pages/Home";
 import "@pages/Rules";
 import "@pages/Game";
 import "@pages/Result";
+import { handleRoute } from "./router/router";
+import { state } from "./state/state";
 
-// Arrancamos el programa y analizamos la ruta
+// Arrancamos el programa y analizamos la ruta y verificamos si hay una sesión guardada
 window.addEventListener("load", () => {
   handleRoute(location.pathname);
+  const storedState = localStorage.getItem("stateData");
+  if (storedState) {
+    state.setState(JSON.parse(storedState));
+  }
 });
 
 // Analizamos el cambio de ruta para cargar la nueva vista
