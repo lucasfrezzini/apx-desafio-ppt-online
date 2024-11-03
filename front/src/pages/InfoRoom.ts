5c46815e374702587bc4050ff3c1a4caf3795a7f
Lucas Frezzini <luquiitas.f@gmail.com>
2024-11-03 05:08:44 -0300

    Feat: make /InfoRoom dinamic

--- front/src/pages/InfoRoom.ts
+++ front/src/pages/InfoRoom.ts
@@ -1,20 +1,33 @@
+import { state } from "@/state/state";
+
 export function initInfoRoom() {
+  const currentState = state.getState();
   const infoRoom = document.createElement("section");
   infoRoom.classList.add("infoRoom");
+
+  const ownerName = currentState.owner.name;
+  const ownerWins = currentState.owner.current_game_wins;
+  const ownerData = ownerName ? `${ownerName}: ${ownerWins}` : "";
+
+  const guestName = currentState.guest.name;
+  const guestWins = currentState.guest.current_game_wins;
+  const guestData = guestName ? `${guestName}: ${guestWins}` : "";
+
+  const roomId = currentState.roomId;
 
   infoRoom.innerHTML = `
   <header class="detailRoom">
     <div>
-      <h3>Marce: 0</h3>
-      <h3>Paula: 0</h3>
+      <h3>${ownerData}</h3>
+      <h3>${guestData}</h3>
     </div>
     <div>
-      <h3>Sala <br/> <span>76HH23<span> </h3>
+      <h3>Sala <br/> <span>${roomId}<span> </h3>
     </div>
   </header>
   <h2>
     Compartí el código:
-    <span>76HH23</span>
+    <span>${roomId}</span>
     con tu contrincante
   </h2>
   <button-el to="/rules">¡Ver las reglas!</button-el>
