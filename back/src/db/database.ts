962f0fa92ce46b7b7f8ec1165fa1736fa020def7
Lucas Frezzini <luquiitas.f@gmail.com>
2024-11-12 20:57:46 -0300

    feat: add dotenv and configure env file

--- back/src/db/database.ts
+++ back/src/db/database.ts
@@ -1,40 +1,27 @@
-// import admin from "firebase-admin";
-// import serviceAccount from "./service-account.json";
-// import { getFirestore } from "firebase/firestore";
-
-//agregar los tipos para inicializar "as admin.ServiceAccount"
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-//   databaseURL: "https://apx-n2-backend-1-default-rtdb.firebaseio.com",
-// });
-
-// Exporta conexión con base de datos Firestore
-// export const firestoreDB = admin.firestore();
-// export const firestoreDB = getFirestore(admin);
-
-// import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
-// import { getFirestore } from "firebase-admin/firestore";
-
-// Inicializa la aplicación con la cuenta de servicio
-// const app = initializeApp({
-//   credential: cert(serviceAccount as ServiceAccount),
-// });
-
-// export const firestoreDB = getFirestore(app);
-// console.log("Conexión a Firestore Establecida");
-
-// import { initializeApp as adminInitializeApp } from "firebase-admin/app";
-// import { getFirestore as adminGetFirestore } from "firebase-admin/firestore";
-
-import { initializeApp, applicationDefault } from "firebase-admin/app";
+import { initializeApp } from "firebase-admin/app";
 import { getDatabase } from "firebase-admin/database";
 import { getFirestore } from "firebase-admin/firestore";
 import * as admin from "firebase-admin";
-import serviceAccount from "./service-account.json";
+
+import "dotenv/config";
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 const app = initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  databaseURL: "https://apx-n2-backend-1-default-rtdb.firebaseio.com", // Reemplaza con tu URL de Realtime Database
+  databaseURL: process.env.DATABASE_URL, // Reemplaza con tu URL de Realtime Database
 });
 
 export const realtimeDB = getDatabase(app);
