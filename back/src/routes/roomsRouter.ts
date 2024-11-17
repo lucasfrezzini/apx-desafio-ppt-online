83870fabd215c57d320461600ec333b672726f83
Lucas Frezzini <luquiitas.f@gmail.com>
2024-11-17 14:32:16 -0300

    delete: quit unused endpoints and fn in Routes Rooms

--- back/src/routes/roomsRouter.ts
+++ back/src/routes/roomsRouter.ts
@@ -5,7 +5,6 @@ import { generateRandomString, whoWins } from "../utils/utils";
 import { v4 as uuidv4 } from "uuid";
 import { dataNewRoomValidator } from "../middlewares/validators/dataNewRoomMiddleware";
 import { dataGuestRoomValidator } from "../middlewares/validators/dataGuestRoomMiddleware";
-import { dataChoicesValidator } from "../middlewares/validators/dataChoicesMiddleware";
 import { ValidationError } from "../utils/customErrors";
 
 // Referencias DB
@@ -143,73 +142,7 @@ roomsRouter.post(
   }
 );
 
-// POST /rooms/:roomId/choices para agregar un choice a ese room
-roomsRouter.post(
-  "/:roomId/choices",
-  dataChoicesValidator,
-  async (req: any, res: any, next: any) => {
-    try {
-      // Llega un id de room, un owner o guest y una choice
-      const { choice, owner } = req.body;
-      const { roomId } = req.params;
-
-      // Obtenemos referencias a las diferentes BD
-      const room = await roomsRef.doc(roomId).get();
-      const { rtdbRoomId } = room.data()!;
-      const roomRTDBRef = await realtimeDB.ref(`roomsPPT/${rtdbRoomId}`);
-      const snapshot = await roomRTDBRef.get();
-
-      // Obtenemos las ultimas choices y el scoreboard
-      const oldChoices = await snapshot.val().lastRoundChoices;
-      const scoreboard = await snapshot.val().scoreboard;
-
-      // Actualizamos las diferentes choices
-      if (owner) {
-        oldChoices.owner = choice;
-        oldChoices.ownerSelected = true;
-      } else {
-        oldChoices.guest = choice;
-        oldChoices.guestSelected = true;
-      }
-
-      // Sí ambos seleccionaron evaluamos ganador de ronda y actualizamos scoreboard
-      if (oldChoices.ownerSelected && oldChoices.guestSelected) {
-        oldChoices.round += 1;
-        oldChoices.ownerSelected = false;
-        oldChoices.guestSelected = false;
-        if (whoWins(oldChoices.owner, oldChoices.guest) == 1) {
-          scoreboard.owner += 1;
-        } else if (whoWins(oldChoices.owner, oldChoices.guest) == 2) {
-          scoreboard.guest += 1;
-        }
-      }
-
-      console.log("oldChoices", oldChoices);
-      console.log("scoreboard", scoreboard);
-
-      // Actualizamos RTDB
-      await roomRTDBRef.update({
-        lastRoundChoices: oldChoices,
-        scoreboard,
-      });
-
-      // Actualizamos Firestore
-      await roomsRef.doc(roomId).update({
-        lastGame: {
-          owner: scoreboard.owner,
-          guest: scoreboard.guest,
-        },
-      });
-      res.status(200).json({
-        success: true,
-        data: "Choices actualizados en Firestore y RTDB correctamente",
-      });
-    } catch (error) {
-      return next(new Error("Error al actualizar choices en la BD"));
-    }
-  }
-);
-// POST /rooms/:roomId/rtdb obtiene el state completo del rtdb
+// POST /rooms/:roomId/rtdb obtiene el identificador del rtdb
 roomsRouter.post("/:roomId/rtdb", async (req: any, res: any, next: any) => {
   const { roomId } = req.params;
 
@@ -238,7 +171,7 @@ roomsRouter.post("/:roomId/rtdb", async (req: any, res: any, next: any) => {
   }
 });
 
-// POST /rooms/:roomId/save guarda el state completo en el rtdb
+// POST /rooms/:roomId/save guarda el state parcial o incompleto en el rtdb
 roomsRouter.post("/:roomId/save", async (req: any, res: any, next: any) => {
   const { roomId: rtdbRoomId } = req.params;
   const { state } = req.body;
@@ -267,7 +200,7 @@ roomsRouter.post("/:roomId/save", async (req: any, res: any, next: any) => {
   }
 });
 
-// POST /rooms/:roomId/rtdb obtiene el state completo del rtdb
+// POST /rooms/:roomId/state obtiene el state completo del rtdb
 roomsRouter.post("/:roomId/state", async (req: any, res: any, next: any) => {
   const { roomId } = req.params;
 
